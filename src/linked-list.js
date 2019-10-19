b3df06e29033394f0b837f7f14ac7eb3e3449087
Darya Zelenkova <daryazeliankova@yandex.ru>
2019-10-19 17:49:03 +0300

    feat:implement doubly linked list data structure

--- src/linked-list.js
+++ src/linked-list.js
@@ -1,27 +1,166 @@
 const Node = require('./node');
 
 class LinkedList {
-    constructor() {}
+    constructor() {
+        this._head = null;
+        this._tail = null;
+        this.length = 0;
+    }
 
-    append(data) {}
+    append(data) {
+        let node = new Node(data);
+        if (!this._head) {
+            this._head = node;
+            this._tail = node;
+        }
+        else {
 
-    head() {}
+            node.prev = this._tail;
+            this._tail.next = node;
+            this._tail = node
+        }
+        this.length++;
+        return this;
+    }
 
-    tail() {}
+    head() {
+        return this._head.data
+    }
 
-    at(index) {}
+    tail() {
+        return this._tail.data
+    }
 
-    insertAt(index, data) {}
+    at(index) {
+        let current = this._head
+        let counter = 1
+        if (index == 0) {
+            return current.data
+        }
+        else {
+            while (current) {
+                current = current.next
+                if (counter == index) {
+                    return current.data
+                }
+                counter++
+            }
+        }
 
-    isEmpty() {}
+    }
 
-    clear() {}
+    insertAt(index, data) {
+        let current = this._head;
+        let counter = 1;
+        let node = new Node(data)
+        if (index == 0) {
+            this._head.prev = node;
+            node.next = this._head;
+            this._head = node
+        }
+        else {
+            while (current) {
+                current = current.next
+                if (counter == index) {
+                    current.prev.next = node
+                    current.prev = node;
+                    node.prev = current.prev;
+                    node.next = current;
+                }
+                counter++
+            }
+        }
+        return this
+    }
 
-    deleteAt(index) {}
+    isEmpty() {
+        if (this._tail && this._head) {
+            return false
+        }
+        else {
+            return true
+        }
+    }
 
-    reverse() {}
+    clear() {
+        let current = this._head;
+        while (current) {
+            debugger
+            current = current.next;
+            if (current != this._tail && current != null) {
+                current.prev.next = null;
+                current.prev = null;
+            }
+        }
+        this._head.data = null
+        this._tail.prev = null
+        this._tail.data = null
+        this.length = 0
+        return this
+    }
 
-    indexOf(data) {}
+    deleteAt(index) {
+        let current = this._head;
+        let counter = 1;
+        if (index == 0) {
+            if (this._head.next) {
+                this._head = this._head.next;
+                this._head.prev = null
+            }
+            else {
+                this._head.data = null
+            }
+        }
+        else {
+            while (current) {
+                current = current.next
+                if (counter == index) {
+                    current.next.prev = current.prev;
+                    current.prev.next = current.next;
+                    current = current.next
+                }
+                counter++
+            }
+        }
+        return this
+    }
+
+    reverse() {
+        let current = this._head;
+        let prev = null;
+        while (current) {
+            let next = current.next
+            current.next = prev
+            current.prev = next
+            prev = current
+            current = next
+        }
+        this._tail = this._head
+        this._head = prev
+        return this
+    }
+
+    indexOf(data) {
+        let current = this._head
+        let counter = 1
+        if (current.data == data) {
+            return 0
+        }
+        else {
+            while (current) {
+                current = current.next
+                if (current && current.data == data) {
+                    return counter
+                }
+                counter++
+                return -1
+
+            }
+
+        }
+
+    }
+
 }
 
 module.exports = LinkedList;
